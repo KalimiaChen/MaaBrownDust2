8379816dd6108d6c03b72fa456327c9d9ad23c68
KalimiaChen <531341547@qq.com>
2025-11-11 00:27:56 +0800

    feat: python整合测试 for v0.1.35

--- agent/main.py
+++ agent/main.py
@@ -1,8 +1,11 @@
 import sys
-
+from pathlib import Path
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
-
+agent_dir = Path(__file__).parent  
+if str(agent_dir) not in sys.path:  
+    sys.path.insert(0, str(agent_dir))  
+    
 import my_actions.my_action
 import my_recognitions.my_reco
 import my_recognitions.圣石选择
